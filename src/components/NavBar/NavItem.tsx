022d861fb94039f9db996234a04d26de3c3e20f7
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2021-10-13 13:28:56 +0200

    NavItem height modified

--- src/components/NavBar/NavItem.tsx
+++ src/components/NavBar/NavItem.tsx
@@ -3,11 +3,11 @@ import React, { FC } from "react";
 
 const NavItem: FC = ({ children }) => {
   return (
-    <Box _hover={{ bg: "white", color: "black" }}>
-      <Link _hover={{ textDecoration: "none" }}>
-        <Text>{children}</Text>
-      </Link>
-    </Box>
+    <Link>
+      <Box lineHeight={10} _hover={{ bg: "white", color: "black" }}>
+        {children}
+      </Box>
+    </Link>
   );
 };
 
