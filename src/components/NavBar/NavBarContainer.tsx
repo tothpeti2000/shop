54672ecf8fdbea68db851b20da56ebb762ce87b3
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2021-10-19 08:23:57 +0200

    Scroll effect added to NavBar

--- src/components/NavBar/NavBarContainer.tsx
+++ src/components/NavBar/NavBarContainer.tsx
@@ -1,16 +1,42 @@
 import { Flex } from "@chakra-ui/react";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 const NavBarContainer: FC = ({ children }) => {
+  const startHeight = "100px";
+  const shrinkedHeight = "70px";
+
+  const startFontSize = "lg";
+  const shrinkedFontSize = "md";
+
+  const [height, SetHeight] = useState(startHeight);
+  const [fontSize, SetFontSize] = useState(startFontSize);
+
+  const Shrink = () => {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      SetHeight(shrinkedHeight);
+      SetFontSize(shrinkedFontSize);
+    } else {
+      SetHeight(startHeight);
+      SetFontSize(startFontSize);
+    }
+  };
+
+  window.onscroll = Shrink;
+
   return (
     <Flex
       as="nav"
       align="center"
       px="10%"
-      h="70px"
+      h={height}
       bgColor="black"
       color="white"
       sx={{ pos: "sticky", top: "0" }}
+      fontSize={fontSize}
+      transition="0.5s"
     >
       {children}
     </Flex>
