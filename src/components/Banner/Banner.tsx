c4834861cd539659b7c6182127df18dbafe6970d
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2021-10-19 09:37:54 +0200

    Banner layout created

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,14 +2,37 @@ import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Banner = () => {
+  const logo = `${process.env.PUBLIC_URL}/images/mercedes.jpg`;
+
   return (
-    <Box h="1000px" bgColor="gray.800" color="white">
-      <Flex direction="column" align="center" justify="center">
-        <Heading>Logo</Heading>
-        <Text>AWESOME SNEAKERS</Text>
-        <Button>SHOP NOW</Button>
+    <Flex
+      h="800px"
+      bgImage={logo}
+      bgRepeat="no-repeat"
+      bgPosition="center"
+      bgAttachment="fixed"
+      bgSize="cover"
+      bgColor="gray.800"
+      color="white"
+      align="center"
+      justify="center"
+    >
+      <Flex direction="column" align="center">
+        <Heading fontSize={100}>Logo</Heading>
+        <Text p={5} fontSize={50}>
+          AWESOME SNEAKERS
+        </Text>
+        <Button
+          p={5}
+          fontSize={40}
+          color="black"
+          _hover={{ bgColor: "black", color: "white" }}
+          transition="0.5s"
+        >
+          SHOP NOW
+        </Button>
       </Flex>
-    </Box>
+    </Flex>
   );
 };
 
