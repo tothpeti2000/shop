defdfff5d495e98b52b262fe0d846122ac1b7a40
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2021-10-19 10:07:12 +0200

    React-awesome-reveal added

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,16 +2,19 @@ import { Flex, Heading } from "@chakra-ui/layout";
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import CategoryCard from "./CategoryCard";
+import Fade from "react-awesome-reveal";
 
 const Categories = () => {
   return (
     <Box bg="grey" color="white" p={10} textAlign="center">
       <Heading>Shop categories</Heading>
-      <Flex p={5}>
-        <CategoryCard>Men's collection</CategoryCard>
-        <CategoryCard>Accessories</CategoryCard>
-        <CategoryCard>Women's collection</CategoryCard>
-      </Flex>
+      <Fade>
+        <Flex p={5}>
+          <CategoryCard>Men's collection</CategoryCard>
+          <CategoryCard>Accessories</CategoryCard>
+          <CategoryCard>Women's collection</CategoryCard>
+        </Flex>
+      </Fade>
     </Box>
   );
 };
