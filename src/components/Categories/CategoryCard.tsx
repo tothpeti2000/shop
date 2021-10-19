e2f8c557429db23f92b94ec334d97bc105772fd6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2021-10-19 08:23:31 +0200

    Categories made clickable

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -1,19 +1,21 @@
-import { Box, Circle, Heading, Text } from "@chakra-ui/layout";
+import { Box, Circle, Heading, Link, Text } from "@chakra-ui/layout";
 import React, { FC } from "react";
 
 const CategoryCard: FC = ({ children }) => {
   return (
     <Box flex="1" textAlign="center" p={5}>
-      <Circle
-        size="300px"
-        bg="white"
-        color="black"
-        boxShadow="dark-lg"
-        m="auto"
-        mb={5}
-      >
-        <Heading>Image</Heading>
-      </Circle>
+      <Link href="#">
+        <Circle
+          size="300px"
+          bg="white"
+          color="black"
+          boxShadow="dark-lg"
+          m="auto"
+          mb={5}
+        >
+          <Heading>Image</Heading>
+        </Circle>
+      </Link>
       <Text fontSize="xl">{children}</Text>
     </Box>
   );
